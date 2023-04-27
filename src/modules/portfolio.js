f781631d878057c735cafe5879f6f0053f03928d
Botlhale Setou <botlhale.setou@gmail.com>
2023-04-27 15:08:42 +0200

    complete portfolio links & pics

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -9,8 +9,8 @@ export const portfolio = {
 		'Mapfrica',
 		'Space-Travelers Hub',
 		'Leaderboard',
-		'Kais Castle',
-		'To-Do List',
+		'Math Magicians',
+		'Awesome Books',
 		'Mens Conference'],
 
 	desc: [`
@@ -24,35 +24,50 @@ export const portfolio = {
 		<br><br>
 		Built With: React, Redux, Rest Countries API
 		`,
-		'The (un)official BNK Chicken website. Work in progress...',
-		'A simple web-library of "awesome books."\nStore and remove from local storage.',
-		'A React game (based on Flappy Bird) I made for my son.',
-		'A leaderboard webapp built using a Microverse API to store and retrieve data.',
-		'The 2023 Men\'s Conference page.\n\n\nComplete with the day\'s program and guest speakers.'],
+		`Space Travelers Hub is a React & Redux built website that uses the real SpaceX API to display rockets and missions - including the functionality of booking & reserving them.
+		<br><br>
+		Built With: React, Redux, SpaceX API
+		`,
+		`Leaderboard is a website that displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.
+		<br><br>
+		Built With: JavaScript, Webpack, HTML/CSS
+		`,
+		`Math-Magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random math-related quote.
+		<br><br>
+		Built With: React, JavaScript, HTML/CSS
+		`,
+		`My Awesome Books is a database web-app of awesome books created by the user and saves data even while offline.
+		<br><br>
+		Built With: JavaScript, HTML/CSS
+		`,
+		`The 2023 Men\'s Conference page.\n\n\nComplete with the day\'s program and guest speakers.
+		<br><br>
+		Built With: JavaScript, HTML/CSS
+		`],
 
 	images: [['./src/artwork/portfolio/recipe-app2.PNG'],
 		['./src/artwork/portfolio/mapfrica1.png'],
-		['./src/artwork/portfolio/bnkc-home.png'],
-		['./src/artwork/portfolio/ab1.png'],
-		['./src/artwork/portfolio/dp.png'],
+		['./src/artwork/portfolio/space-travellers-hub.png'],
 		['./src/artwork/portfolio/l1.png'],
+		['./src/artwork/portfolio/math.png'],
+		['./src/artwork/portfolio/ab.png'],
 		['./src/artwork/portfolio/mc1.png']],
 
-	live: ['',
+	live: ['https://recipe-app-0tvs.onrender.com/',
 	'http://mapfrica.onrender.com/',
-	'',
-	'',
-	'',
-	'',
-	''],
+	'https://space-travellers-hub-naaj.onrender.com/',
+	'https://leaderboard-r5l2.onrender.com/',
+	'https://math-magicians-ysoy.onrender.com/',
+	'http://awesomebooks.atwebpages.com/',
+	'https://mensconference.000webhostapp.com/'],
 
 	source: ['https://github.com/Botlhale-Setou/Recipe-App',
 	'https://github.com/Botlhale-Setou/react-capstone-project',
-	'',
-	'',
-	'',
-	'',
-	'']
+	'https://github.com/Botlhale-Setou/space-travelers-hub',
+	'https://github.com/Botlhale-Setou/Leaderboard',
+	'https://github.com/Botlhale-Setou/Math-Magicians',
+	'https://github.com/Botlhale-Setou/awesome-books-app',
+	'https://github.com/Botlhale-Setou/mens-conference']
 };
 
 export default function loadPortfolio() {
